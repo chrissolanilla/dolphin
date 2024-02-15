937a79870b7f47a82c5d965c08a07e40df410449
Jared M. White <jwhite2292forward@gmail.com>
2024-02-15 17:00:23 -0600

    Pad buffer fixes

--- Source/Core/Core/Brawlback/MemRegions.h
+++ Source/Core/Core/Brawlback/MemRegions.h
@@ -110,7 +110,8 @@ struct MemRegions
       {0x81601960, 0x81734d60, nullptr, "InfoInstance"},
       {0x814ce460, 0x8154e560, nullptr, "StageInstance"},
       {0x8049e57c, 0x8049e57c + 0xC, nullptr, "g_grCollisionList"},
-      {0x80b8db60, 0x80c23a60, nullptr, "Effect"}
+      {0x80b8db60, 0x80c23a60, nullptr, "Effect"},
+      {0x805ba480, 0x805baca0, nullptr, "Pad Buffer"}
     };
   }
 };
